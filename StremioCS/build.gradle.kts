e7ca817e969cd2f2cd58d1c971fea2941266a10b
rougegz <thegnsme@hotmail.com>
2026-09-16 14:15:26 +0530

    Update build.gradle.kts

--- StremioCS/build.gradle.kts
+++ StremioCS/build.gradle.kts
@@ -1,7 +1,7 @@
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
-version = 6
+version = 7
 cloudstream {
     description = """
         Watch anything with your own Stremio addons. Open Settings to add them.
